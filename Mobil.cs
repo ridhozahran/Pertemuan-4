46930f0a3a0f0ec336d0d0399c0dd58184619d01
ridhozahranp <ridhozahranp@students.amikom.ac.id>
2023-07-11 11:10:10 +0700

    first commit

---
+++ Mobil.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Mobil
+    {
+         //fields
+        private string _Warna;
+        private float _JumlahPintu;
+        private string _Merek;
+        private string _Model;
+        private float _TahunKeluaran;
+
+        //properti
+        private string warna
+        {
+            get
+            {
+                return _Warna;
+            }
+            set
+            {
+                _Warna = value;
+            }
+        }
+        private float jumlahPintu
+        {
+            get
+            {
+                return _JumlahPintu;
+            }
+            set
+            {
+                _JumlahPintu = value;
+            }
+        }
+        private string merek
+        {
+            get
+            {
+                return _Merek;
+            }
+            set
+            {
+                _Merek = value;
+            }
+        }
+        private string model
+        {
+            get
+            {
+                return _Model;
+            }
+            set
+            {
+                _Model = value;
+            }
+        }
+        private float tahunKeluaran
+        {
+            get
+            {
+                return _TahunKeluaran;
+            }
+            set
+            {
+                _TahunKeluaran = value;
+            }
+        }
+        public void HitungKecepatan(int jarak, int waktu)
+        {
+            double kecepatan = (double)jarak / waktu;
+            Console.WriteLine($"Kecepatan mobil adalah {kecepatan} km/jam\n");
+        }
+
+        public void Klakson1()
+        {
+            Console.WriteLine("bunyi klakson : Tinnnn! Tinnnn!");
+            Console.WriteLine("===================================\n");
+        }
+        public void Klakson2()
+        {
+            Console.WriteLine("bunyi klakson : beepppp! beepppp!");
+            Console.WriteLine("===================================\n");
+        }
+        public void Klakson3()
+        {
+            Console.WriteLine("bunyi klakson :Trenkkk! Trenkkk!");
+            Console.WriteLine("===================================\n");
+        }
+        public void TampilInfo()
+        {
+            Console.WriteLine($"Merek Mobil    : {_Merek} \n");
+            Console.WriteLine($"Dengan model   : {_Model} \n");
+            Console.WriteLine($"Variasi warna  : {_Warna} \n");
+            Console.WriteLine($"Jumlah pintu   : {_JumlahPintu} pintu\n");
+            Console.WriteLine($"Keluaran tahun : {_TahunKeluaran}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil1 = new Mobil();
+
+            // Set properti mobil
+            mobil1._Warna = "Putih";
+            mobil1._JumlahPintu = 2;
+            mobil1._Merek = "lamborgini";
+            mobil1._Model = "Veneno Roadster";
+            mobil1._TahunKeluaran = 2013;
+
+            // Panggil method mobil
+            mobil1.TampilInfo();
+            mobil1.Klakson1();
+            mobil1.HitungKecepatan(710, 2);
+
+            Mobil mobil2 = new Mobil();
+
+            // Set properti mobil
+            mobil2._Warna = "Hitam";
+            mobil2._JumlahPintu = 2;
+            mobil2._Merek = "Bugatti";
+            mobil2._Model = "Chiron";
+            mobil2._TahunKeluaran = 2018;
+
+            // Panggil method mobil
+            mobil2.TampilInfo();
+            mobil2.Klakson2();
+            mobil2.HitungKecepatan(620, 2);
+
+            Mobil mobil3 = new Mobil();
+
+            // Set properti mobil
+            mobil3._Warna = "Merah";
+            mobil3._JumlahPintu = 2;
+            mobil3._Merek = "Ferari";
+            mobil3._Model = "J50";
+            mobil3._TahunKeluaran = 2016;
+
+            // Panggil method mobil
+            mobil3.TampilInfo();
+            mobil3.Klakson3();
+            mobil3.HitungKecepatan(1028, 2);
+
+            Console.WriteLine("tekan random key untuk menghentikan program\n");
+
+
+            Console.ReadKey();
+        }
+    }
+}
